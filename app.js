a6cb37d29a45aab0b757fb94dbfc2b248d76784d
Danny Brenes <dbrenes7@estudiantec.cr>
2022-10-23 12:50:06 -0600

    CRUD Catalogo
    Ya esta toda la progra terminada

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const session = driver.session()
 app.listen(3000);
 console.log('El servidor esta en el puerto 3000');
 
+// Muestra el menú de inicio del programa
 app.get('/',function(req,res){
     res.render('menuInicio')
 })
@@ -66,9 +67,10 @@ app.post('/crearClientes',function(req,res){
 
 // Buscar un cliente especifico en la base de datos
 app.post('/buscarCliente',function(req,res){
-    var id = req.body.id;
+    var name = req.body.first_name;
+    var lastname = req.body.last_name;
     session
-        .run('MATCH (n:Cliente{id:$idParam}) RETURN n',{idParam:id})
+        .run('MATCH (n:Cliente{first_name:$firstNameParam,last_name:$lastNameParam}) RETURN n',{firstNameParam:name,lastNameParam:lastname})
         .then(function(result){
             var clientesArr = [];
             result.records.forEach(function(record){
@@ -201,6 +203,29 @@ app.post('/view/consultas/productoComun',function(req,res){
         })
 })
 
+// Muestra las marcas existentes
+app.get('/mostrarMarcas',function(req,res){
+    session
+        .run('MATCH (n:Marca) RETURN n')
+        .then(function(result){
+            var marcasArr = [];
+            result.records.forEach(function(record){
+                console.log(record._fields[0].properties)
+                marcasArr.push({
+                    id: record._fields[0].properties.id,
+                    nombre: record._fields[0].properties.nombre,
+                    pais: record._fields[0].properties.pais 
+                });
+            });
+            res.render('indexMarcas',{
+                marcas: marcasArr
+            });
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+})
+
 // Muestra menu de consultas disponibles
 app.get('/view/mostrarConsultas',function(req,res){
     res.render('indexConsultas')
@@ -209,7 +234,8 @@ app.get('/view/mostrarConsultas',function(req,res){
 // Muestra consulta de top cinco productos vendidos
 app.get('/view/consultas/top5Productos',function(req,res){
     session
-        .run('match (n:Cliente)-[r:Compra]-(p:Producto) return p as Productos_Mayor_Cantidad_Vendidas, count(*) as Unidades_Vendidas order by Unidades_Vendidas desc limit 5')
+        .run('match(x:Cliente)-[r:Compra]->(y:Producto) return y as Productos_Mayor_Cantidad_Vendidas, sum(toInteger(r.cantidad)) as Cantidad_Compras order by Cantidad_Compras desc limit 5')
+        //.run('match (n:Cliente)-[r:Compra]-(p:Producto) return p as Productos_Mayor_Cantidad_Vendidas, count(*) as Unidades_Vendidas order by Unidades_Vendidas desc limit 5')
         .then(function(result){
             var productosArr = [];
             result.records.forEach(function(record){
@@ -231,7 +257,8 @@ app.get('/view/consultas/top5Productos',function(req,res){
 // Muestra consulta de top cinco mejores clientes
 app.get('/view/consultas/top5Clientes',function(req,res){
     session
-        .run('match(x:Cliente)-[r:Compra]->(y:Producto) return x as Clientes_Mayor_Cantidad_Compras, count(*) as Cantidad_Compras order by Cantidad_Compras desc limit 5')
+        //.run('match(x:Cliente)-[r:Compra]->(y:Producto) return x as Clientes_Mayor_Cantidad_Compras, count(*) as Cantidad_Compras order by Cantidad_Compras desc limit 5')
+        .run('match(x:Cliente)-[r:Compra]->(y:Producto) return x as Clientes_Mayor_Cantidad_Compras, sum(toInteger(r.cantidad)) as Cantidad_Compras order by Cantidad_Compras desc limit 5')
         .then(function(result){
             var clientesArr = [];
             result.records.forEach(function(record){
@@ -253,7 +280,8 @@ app.get('/view/consultas/top5Clientes',function(req,res){
 // Muestra consulta de top cinco mejores marcas
 app.get('/view/consultas/top5Marcas',function(req,res){
     session
-        .run('match (c:Cliente)-[r:Compra]-(p:Producto) return p.marca as marca, count(p.marca) as count order by count desc limit 5')
+        //.run('match (c:Cliente)-[r:Compra]-(p:Producto) return p.marca as marca, count(p.marca) as count order by count desc limit 5')
+        .run('match(x:Cliente)-[r:Compra]->(y:Producto) return y.marca as Marcas_Mayor_Cantidad_Vendidas, sum(toInteger(r.cantidad)) as Unidades_Vendidas order by Unidades_Vendidas desc limit 5')
         .then(function(result){
             var marcasArr = [];
             result.records.forEach(function(record){
@@ -271,6 +299,7 @@ app.get('/view/consultas/top5Marcas',function(req,res){
         })
 })
 
+// Muestra la consulta especializada dos
 app.post('/view/consultas/especializada2',function(req,res){
     var idCliente = req.body.id;
     session
@@ -290,6 +319,105 @@ app.post('/view/consultas/especializada2',function(req,res){
         })
         .catch(function(err){
             console.log(err);
+        })
+})
+
+// Muestra todos los productos registrados
+app.get('/view/mostrarCatalogo',function(req,res){
+    session
+        .run('MATCH (n:Producto) RETURN n')
+        .then(function(result){
+            var CatalogoArr = [];
+            result.records.forEach(function(record){                
+                //console.log(record._fields[0].properties)
+                CatalogoArr.push({
+                    id: record._fields[0].properties.id,
+                    nombre: record._fields[0].properties.nombre,
+                    marca: record._fields[0].properties.marca,
+                    precio: record._fields[0].properties.precio  
+                    
+                });
+            });
+            res.render('indexCatalogo',{
+                objCatalogo: CatalogoArr
+            });
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+})
+
+// Crea un nuevo producto
+app.post('/crearProducto',function(req,res){
+    var id = req.body.id;
+    var nombre = req.body.nombre;
+    var marca = req.body.marca;
+    var precio = req.body.precio;
+    console.log(nombre);
+    session
+        .run('CREATE (p:Producto {id:$idParam,nombre:$nombreParam,marca:$marcaParam,precio:$precioParam}) RETURN p',
+            {idParam:id,nombreParam:nombre,marcaParam:marca,precioParam:precio})
+        .then(function(result){
+            res.redirect('/view/mostrarCatalogo');
+        })
+        .catch(function(err){
+            console.log(err)
+        })
+})
+
+// Busca un producto en específico
+app.post('/buscarProducto',function(req,res){
+    var id = req.body.id;
+    session
+        .run('MATCH (n:Producto{id:$idParam}) RETURN n',{idParam:id})
+        .then(function(result){
+            var CatalogoArr = [];
+            result.records.forEach(function(record){
+                //console.log(record._fields[0].properties)
+                CatalogoArr.push({
+                    id: record._fields[0].properties.id,
+                    nombre: record._fields[0].properties.nombre,
+                    marca: record._fields[0].properties.marca,
+                    precio: record._fields[0].properties.precio  
+                });
+            });
+            res.render('indexCatalogo',{
+                objCatalogo: CatalogoArr
+            });
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+})
+
+// Elimina un producto específico
+app.post('/eliminarProducto',function(req,res){
+    var id = req.body.id;
+    session
+        //.run('MATCH (n:Producto{id:$idParam})-[r:Compra]-() DELETE r,n',{idParam:id})
+        .run('MATCH (n:Producto{id:$idParam}) DELETE n',{idParam:id})
+        .then(function(result){
+            res.redirect('/view/mostrarCatalogo');
+        })
+        .catch(function(err){
+            console.log(err);
+        })
+})
+
+// Edita un producto específico
+app.post('/editarProducto',function(req,res){
+    var id = req.body.id;
+    var nombre = req.body.nombre;
+    var marca = req.body.marca;
+    var precio = req.body.precio;
+    session
+        .run('MATCH (c:Producto {id:$idParam}) set c={id:$idParam,nombre:$nombreParam,marca:$marcaParam,precio:$precioParam} RETURN c',
+            {idParam:id,nombreParam:nombre,marcaParam:marca,precioParam:precio})
+        .then(function(result){
+            res.redirect('/view/mostrarCatalogo');
+        })
+        .catch(function(err){
+            console.log(err)
         })
 })
 module.exports = app;
